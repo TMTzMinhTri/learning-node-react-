3c928cb1b3bf445392b64c86c8e1e22cff790f24
Trần Minh Trí <tmtzminhtri@gmail.com>
2019-05-30 00:21:35 +0700

    define stucture

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys')
-
 const app = express()
 const port = process.env.PORT || 5000
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-    })
-)
+require('./service/passport')
+
+
+require('./routes/authRoutes')(app)
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/auth/google/callback', passport.authenticate('google', {
-    scope:['profile', 'email']
-}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
