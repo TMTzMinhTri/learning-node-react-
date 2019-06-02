f69a102651ecd2334113c296464eca3fc7cfa3d4
Trần Minh Trí <tmtzminhtri@gmail.com>
2019-06-03 01:43:43 +0700

    create cookie-session

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
 //service
+require('./models/User')
 require('./service/passport')
 require('./service/facebookPassport')
-require('./models/User')
 
-mongoose.connect(keys.mongoDBConnectionString)
+const app = express()
+
+mongoose.connect(keys.mongoDBConnectionStringLocalhost)
+
+app.use(cookieSession({
+    maxAge: 5*60*1000,
+    keys:[keys.cookieKey],
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 //routes
 const authRoutes = require('./routes/authRoutes')
 
-const app = express()
 //handle routes
 app.use('/auth',authRoutes)
 
@@ -18,6 +28,14 @@ app.use('/auth',authRoutes)
 
 // root
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/currentUser', (req, res)=> {
+    res.send(req.session)
+    res.send(req.user)
+})
+app.get('/logout',(req,res) => {
+    req.logOut()
+    res.send(req.user)
+})
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -3,6 +3,14 @@ const { Schema } = mongoose
 
 const userSchema = new Schema({
     googleId: String,
+    name: {
+        familyName: String,
+        givenName: String
+    },
+    email: String,
+    displayName: String,
+    avata: String,
+    username: String
 })
 
-mongoose.model('users',userSchema)
+mongoose.model('users', userSchema)
